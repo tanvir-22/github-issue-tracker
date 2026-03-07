6f8d18ebdfbfd1b2869f304c931fe4d30b948728
tanvir-22 <tanvir.mazumder01@gmail.com>
2026-03-07 23:26:34 +0600

    login functionality added

--- login.js
+++ login.js
@@ -1,1 +1,14 @@
-console.log("welcome to login.js");
+const username = document.getElementById('username');
+const password = document.getElementById('password');
+
+const signin = document.getElementById('signinBtn');
+
+signin.addEventListener('click',()=>{
+    if(username.value==='admin' && password.value==='admin123'){
+        window.location.assign('/dashboard.html')
+        console.log(window.location);
+    }
+    else{
+        alert("Wrong Credential: Try Again!")
+    }
+})
